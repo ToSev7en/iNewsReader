ab5e684a5efc4f4190efb23db5ac90d74969f216
ToSev7en <980798542@qq.com>
2017-01-19 15:26:45 +0800

    add jquery

--- scripts/background.js
+++ scripts/background.js
@@ -1,7 +1,22 @@
 "use strict";
 
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({
+        url: chrome.extensioon.getURL("popup.html")
+    });
+})
+
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.method === 'showAlert') {
         alert('showAlert');
     }
 });
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+            "来自内容脚本：" + sender.tab.url :
+            "来自扩展程序");
+        if (request.greeting == "您好")
+            sendResponse({ farewell: "再见" });
+    });
--- scripts/content_script.js
+++ scripts/content_script.js
@@ -1,50 +1,57 @@
 (function() {
     'use strict';
 
-    var main = function() {
-        var blockedUserLinks = [];
-        var blockedUserNames = [];
-
-        var blockUser = function() {
-            $.each(blockedUserLinks, function(index, item) {
-                var links = [item, "http://www.zhihu.com" + item, "http://www.zhihu.com" + item];
-                $.each(links, function(index, link) {
-                    $('div.zm-item-answer:has(a.author-link[href="' + link + '"])').hide();
-                });
-            });
-
-            $.each(blockedUserNames, function(index, item) {
-                $('div.comment-app-holder div[aria-label="' + item + "的评论" + '"]').hide();
-            });
-
-        };
-
-        // load blocked user
-        $.get("/settings/filter").done(function(html) {
-            var page = $(html);
-            $.each(page.find('.blocked-users a.avatar-link'), function(index, item) {
-                blockedUserLinks.push($(item).attr("href"));
-            });
-
-            $.each(page.find('.blocked-users div.body a'), function(index, item) {
-                blockedUserNames.push($(item).text());
-            });
-
-            blockUser();
-        });
 
 
-        $(document).ajaxComplete(function(event, request, settings) {
-            blockUser();
-        });
-    };
+    // var main = function() {
+    //     var blockedUserLinks = [];
+    //     var blockedUserNames = [];
 
-    chrome.storage.local.get("disabled", function(data) {
-        if (!data || !data["disabled"]) {
-            var injectedScript = document.createElement('script');
-            injectedScript.type = 'text/javascript';
-            injectedScript.text = '(' + main + ')("");';
-            (document.body || document.head).appendChild(injectedScript);
-        }
+    //     var blockUser = function() {
+    //         $.each(blockedUserLinks, function(index, item) {
+    //             var links = [item, "http://www.zhihu.com" + item, "http://www.zhihu.com" + item];
+    //             $.each(links, function(index, link) {
+    //                 $('div.zm-item-answer:has(a.author-link[href="' + link + '"])').hide();
+    //             });
+    //         });
+
+    //         $.each(blockedUserNames, function(index, item) {
+    //             $('div.comment-app-holder div[aria-label="' + item + "的评论" + '"]').hide();
+    //         });
+
+    //     };
+
+    //     // load blocked user
+    //     $.get("/settings/filter").done(function(html) {
+    //         var page = $(html);
+    //         $.each(page.find('.blocked-users a.avatar-link'), function(index, item) {
+    //             blockedUserLinks.push($(item).attr("href"));
+    //         });
+
+    //         $.each(page.find('.blocked-users div.body a'), function(index, item) {
+    //             blockedUserNames.push($(item).text());
+    //         });
+
+    //         blockUser();
+    //     });
+
+
+    //     $(document).ajaxComplete(function(event, request, settings) {
+    //         blockUser();
+    //     });
+    // };
+
+    // chrome.storage.local.get("disabled", function(data) {
+    //     if (!data || !data["disabled"]) {
+    //         var injectedScript = document.createElement('script');
+    //         injectedScript.type = 'text/javascript';
+    //         injectedScript.text = '(' + main + ')("");';
+    //         (document.body || document.head).appendChild(injectedScript);
+    //     }
+    // });
+
+
+    chrome.runtime.sendMessage({ greeting: "您好" }, function(response) {
+        console.log(response.farewell);
     });
 })();
