6f5d7b4376fb5651c3955ef23188b57b1b8e0860
ToSev7en <980798542@qq.com>
2017-01-21 02:16:52 +0800

    query words

---
+++ css/iconfont.js
@@ -0,0 +1,132 @@
+;(function(window) {
+
+  var svgSprite = '<svg>' +
+    '' +
+    '<symbol id="icon-speaker07" viewBox="0 0 1024 1024">' +
+    '' +
+    '<path d="M706.188724 327.778278c-3.38919-9.023514-10.204409-16.352433-18.878976-20.316721-7.100722-3.985778-15.508205-5.714142-24.142863-4.351098-20.537755 3.233648-34.546817 22.682605-31.273261 43.437302 1.852184 11.753695 8.850575 21.398356 18.296715 26.976398 36.256762 38.793538 58.57507 90.756003 58.57507 148.062174 0 62.702064-26.737968 119.027907-69.252247 158.688185-7.883551 6.80294-12.879332 16.853853-12.879332 28.077475 0 20.48045 16.60212 37.08257 37.084617 37.08257 10.482749 0 19.939121-4.356214 26.685779-11.354606 7.198959-4.176113 12.81077-10.786671 15.782451-18.724457 46.461171-51.312666 75.018577-119.091352 75.018577-193.770192C781.207301 446.892143 752.648872 379.12983 706.188724 327.778278L706.188724 327.778278zM835.920414 192.523805c-6.164397-12.558014-19.078521-21.205974-34.015721-21.205974-20.915355 0-37.870516 16.957207-37.870516 37.876656 0 13.732769 7.300266 25.75457 18.23941 32.394804 64.187905 67.167772 103.8226 157.975964 103.8226 258.218016 0 112.235201-49.66719 212.670657-127.959324 281.222963l0.143263 0.170892c-12.286838 6.096858-20.733207 18.764366-20.733207 33.404808 0 20.589944 16.689101 37.275975 37.280068 37.275975 12.227486 0 23.081695-5.885034 29.879518-14.981203 0.331551-0.387833 0.656963-0.782829 0.970094-1.180895l0.098237 0.092098c93.578282-81.900312 152.97916-201.887057 152.97916-336.004637C958.757067 380.669906 911.919319 272.608771 835.920414 192.523805L835.920414 192.523805zM437.431677 128.156822l-186.232518 190.536544-113.505124 0c-48.559972 0-72.445986 26.143427-72.445986 72.461335l0 241.454214c0 45.558615 24.645306 72.466452 72.445986 72.466452l113.506147 0 186.231495 190.519148c53.510728 50.251497 134.940318 28.106128 134.940318-55.32812l0-656.796804C572.370972 99.172697 489.926262 78.960353 437.431677 128.156822L437.431677 128.156822zM499.924987 803.153126c0 54.850236-24.313755 24.095791-49.260937-0.523933-43.48949-42.917462-109.187795-112.269993-163.196873-170.020278l-101.477182 0c-36.718273 0-48.296983-12.314467-48.296983-48.294936l0-144.876622c0-35.223223 12.335956-48.284703 48.296983-48.284703l102.607936 0c53.726645-58.060347 118.78743-127.285988 162.248268-169.815617 24.667819-24.1357 49.078789-55.583946 49.078789 1.523703C499.924987 340.838735 499.924987 687.554318 499.924987 803.153126L499.924987 803.153126z"  ></path>' +
+    '' +
+    '</symbol>' +
+    '' +
+    '<symbol id="icon-iconfontsvg45" viewBox="0 0 1024 1024">' +
+    '' +
+    '<path d="M563.872 113.76c-14.016-6.592-30.56-4.416-42.464 5.472L291.872 310.272 108.928 309.536 108.768 309.536c-10.528 0-20.672 4.16-28.128 11.616-7.52 7.456-11.776 17.6-11.776 28.224l-0.832 319.264c0 10.624 4.16 20.8 11.648 28.288 7.456 7.552 17.632 11.744 28.256 11.744l185.12 0 228.384 190.336c7.296 6.08 16.416 9.216 25.536 9.216 5.76 0 11.52-1.248 16.928-3.776 14.016-6.56 22.976-20.64 22.976-36.128L586.88 149.888C586.88 134.432 577.92 120.352 563.872 113.76L563.872 113.76zM718.624 313.312c-19.68-9.312-43.072-0.832-52.64 18.752-9.536 19.648-1.28 43.424 18.24 53.248 2.528 1.312 62.304 32.384 62.304 120.864 0 94.528-55.904 125.856-57.76 126.88-19.712 9.856-27.712 33.856-17.856 53.568 7.04 14.016 21.088 22.08 35.776 22.08 5.984 0 12.096-1.344 17.792-4.192 4.16-2.112 101.856-52.672 101.856-198.336C826.336 365.632 723.008 315.392 718.624 313.312L718.624 313.312zM842.88 196.096c-18.4-11.808-42.816-6.432-54.848 11.936-11.968 18.368-6.72 43.072 11.584 55.168 4.32 2.848 106.56 72.512 106.56 245.92 0 173.792-98.08 243.52-101.984 246.176-18.368 12.256-23.296 36.992-11.072 55.36 7.68 11.52 20.352 17.76 33.248 17.76 7.584 0 15.296-2.208 22.112-6.72 5.6-3.712 137.504-94.144 137.504-312.608C985.984 290.208 848.736 199.84 842.88 196.096L842.88 196.096zM842.88 196.096"  ></path>' +
+    '' +
+    '</symbol>' +
+    '' +
+    '</svg>'
+  var script = function() {
+    var scripts = document.getElementsByTagName('script')
+    return scripts[scripts.length - 1]
+  }()
+  var shouldInjectCss = script.getAttribute("data-injectcss")
+
+  /**
+   * document ready
+   */
+  var ready = function(fn) {
+    if (document.addEventListener) {
+      if (~["complete", "loaded", "interactive"].indexOf(document.readyState)) {
+        setTimeout(fn, 0)
+      } else {
+        var loadFn = function() {
+          document.removeEventListener("DOMContentLoaded", loadFn, false)
+          fn()
+        }
+        document.addEventListener("DOMContentLoaded", loadFn, false)
+      }
+    } else if (document.attachEvent) {
+      IEContentLoaded(window, fn)
+    }
+
+    function IEContentLoaded(w, fn) {
+      var d = w.document,
+        done = false,
+        // only fire once
+        init = function() {
+          if (!done) {
+            done = true
+            fn()
+          }
+        }
+        // polling for no errors
+      var polling = function() {
+        try {
+          // throws errors until after ondocumentready
+          d.documentElement.doScroll('left')
+        } catch (e) {
+          setTimeout(polling, 50)
+          return
+        }
+        // no errors, fire
+
+        init()
+      };
+
+      polling()
+        // trying to always fire before onload
+      d.onreadystatechange = function() {
+        if (d.readyState == 'complete') {
+          d.onreadystatechange = null
+          init()
+        }
+      }
+    }
+  }
+
+  /**
+   * Insert el before target
+   *
+   * @param {Element} el
+   * @param {Element} target
+   */
+
+  var before = function(el, target) {
+    target.parentNode.insertBefore(el, target)
+  }
+
+  /**
+   * Prepend el to target
+   *
+   * @param {Element} el
+   * @param {Element} target
+   */
+
+  var prepend = function(el, target) {
+    if (target.firstChild) {
+      before(el, target.firstChild)
+    } else {
+      target.appendChild(el)
+    }
+  }
+
+  function appendSvg() {
+    var div, svg
+
+    div = document.createElement('div')
+    div.innerHTML = svgSprite
+    svgSprite = null
+    svg = div.getElementsByTagName('svg')[0]
+    if (svg) {
+      svg.setAttribute('aria-hidden', 'true')
+      svg.style.position = 'absolute'
+      svg.style.width = 0
+      svg.style.height = 0
+      svg.style.overflow = 'hidden'
+      prepend(svg, document.body)
+    }
+  }
+
+  if (shouldInjectCss && !window.__iconfont__svg__cssinject__) {
+    window.__iconfont__svg__cssinject__ = true
+    try {
+      document.write("<style>.svgfont {display: inline-block;width: 1em;height: 1em;fill: currentColor;vertical-align: -0.1em;font-size:16px;}</style>");
+    } catch (e) {
+      console && console.log(e)
+    }
+  }
+
+  ready(appendSvg)
+
+
+})(window)
--- scripts/content_script.js
+++ scripts/content_script.js
@@ -1,6 +1,90 @@
 'use strict';
-window.onload = function() {
+
+$(document).ready(function() {
+
+    // var doc = document.body;
+
+    // $("html").remove();
+
+    $("html").removeAttr("id class data-page-path");
+    // $("html").empty();
+
+    // $("style").remove();
+
+    // var html = document.createElement("html");
+    // var head = document.createElement("head");
+    // var body = document.createElement("body");
+    // document.html.appendChild(head);
+    // document.html.appendChild(body);
+
+
+
+    // $("html").promise().done(function() {
+    //     document.appendChild(html);
+    // });
+    // $(document.html).append(
+    //     '<head>' +
+    //     '<title></title>' +
+    //     '<meta charset="UTF-8">' +
+    //     '<link href="//cdn.bootcss.com/normalize/5.0.0/normalize.min.css" rel="stylesheet">' +
+    //     '<link href="css/style.css" rel="stylesheet">' +
+    //     '</head>');
+    // document.appendChild(html);
+    // if (document.readyState == 'complete') {
+
+    // $(document).ajaxComplete(function(event, request, settings) {
+    // document.appendChild(html);
+
+
+    // });
+    // }
+
+
+
+
+    // $(document.children[0]).append(
+    //     '<head>' +
+    //     '<title></title>' +
+    //     '<meta charset="UTF-8">' +
+    //     '<link href="//cdn.bootcss.com/normalize/5.0.0/normalize.min.css" rel="stylesheet">' +
+    //     '<link href="css/style.css" rel="stylesheet">' +
+    //     '</head>');
+
+
+    // $(document).append(
+    // '<html lang="en">' +
+    // '<head>' +
+    // '<title></title>' +
+    // '<meta charset="UTF-8">' +
+    // '<link href="//cdn.bootcss.com/normalize/5.0.0/normalize.min.css" rel="stylesheet">' +
+    // '<link href="css/style.css" rel="stylesheet">' +
+    // '</head>' +
+
+    // '<body>' +
+    // '<div id="app">' +
+    // '</div>' +
+    // '</body>' +
+    // '<script type="text/javascript" src="scripts/popup.js"></script>' +
+    // '</html>'
+    // var 
+    // var boxContainer = document.createElement("head");
+    // document.children[0].appendChild(boxContainer);
+    // "<strong>Hello</strong>"
+    // );
+    // $("").remove();
+    // $("script").remove();
+    // $("noscript").remove();
+    // $("link").remove();
+    // $("meta").remove();
+    // $("style").remove();
+    // $("iframe").remove();
+
+    // $("aside").remove();
+
+
     if (document.URL == "https://www.theguardian.com/politics/2015/may/28/david-cameron-sets-off-on-mission-to-win-over-european-leaders") {
+
+
         if (document.readyState == 'complete') {
             chrome.runtime.sendMessage({
                 method: "speakLouder",
@@ -10,46 +94,114 @@ window.onload = function() {
             });
         }
 
-        document.addEventListener("mouseup", function() {
-            if (window.getSelection().toString() != null && window.getSelection().toString().trim() != "") {
-                // alert(window.getSelection().toString());
-                chrome.runtime.sendMessage({
-                    method: "speakLouder",
-                    data: window.getSelection().toString().trim()
-                }, function(response) {
-
-                });
-
-
-                Util.httpRequest('https://api.shanbay.com/bdc/search/?word=' + window.getSelection().toString().trim(), function(res) {
-                    var json;
-                    if (typeof res === 'object') {
-                        json = res;
-                    } else {
-                        json = eval("(" + res + ")");
-                    }
-
-                    var boxContainer = document.createElement("div");
-
-                    var speaker = document.createElement("audio");
-
-                    var meaning = document.createElement("p");
-
-                    boxContainer.appendChild(speaker);
-
-                    boxContainer.appendChild(meaning);
-                    boxContainer.className = "word-box";
-
-                    document.body.appendChild(boxContainer);
-
-
-                });
-            }
-        })
-
-
     }
-}
+
+    document.addEventListener("mouseup", function() {
+        if (window.getSelection().toString() != null && window.getSelection().toString().trim() != "") {
+            // alert(window.getSelection().toString());
+            chrome.runtime.sendMessage({
+                method: "speakLouder",
+                data: window.getSelection().toString().trim()
+            }, function(response) {
+
+            });
+
+
+            Util.httpRequest('https://api.shanbay.com/bdc/search/?word=' + window.getSelection().toString().trim(), function(res) {
+                var json;
+                if (typeof res === 'object') {
+                    json = res;
+                } else {
+                    json = eval("(" + res + ")");
+                }
+
+                var HTML = '<div class="word-box">' +
+                    '<audio controls="controls" src="http://dict.youdao.com/dictvoice?audio=voice&type=1"></audio>' +
+                    '<div class="word-bar"><a class="wordContent" href="#">' + json.data.content + '</a></div>' +
+                    '<div class="pronunciation">' +
+                    '<ul>' +
+                    '<li>' +
+                    '<i class="iconfont icon-iconfontsvg45 speaker-icon"></i>' +
+                    '<span>英音[<b>' + json.data.pronunciations.uk + '</b>]</span>' +
+                    '</li>' +
+                    '<li>' +
+                    '<i class="iconfont icon-speaker07 speaker-icon"></i>' +
+                    '<span>美音[<b>' + json.data.pronunciations.us + '</b>]</span>' +
+                    '</li>' +
+                    '</ul>' +
+                    '</div>' +
+                    '<div class="cn-definition">' +
+                    '<p>' + json.data.definition + '</p>' +
+                    '</div>' +
+                    '</div>';
+                var body = jQuery("body"),
+                    wb = jQuery(".word-box");
+                if (wb.length === 0) {
+                    body.append(HTML);
+                }
+            });
+        } else {
+            jQuery(".word-box").remove();
+        }
+    })
+
+});
+
+
+
+// window.onload = function() {
+// if (document.URL == "https://www.theguardian.com/politics/2015/may/28/david-cameron-sets-off-on-mission-to-win-over-european-leaders") {
+//     if (document.readyState == 'complete') {
+//         chrome.runtime.sendMessage({
+//             method: "speakLouder",
+//             data: "Chrome Extension Enable"
+//         }, function(response) {
+
+//         });
+//     }
+
+
+
+//     document.addEventListener("mouseup", function() {
+//         if (window.getSelection().toString() != null && window.getSelection().toString().trim() != "") {
+//             // alert(window.getSelection().toString());
+//             chrome.runtime.sendMessage({
+//                 method: "speakLouder",
+//                 data: window.getSelection().toString().trim()
+//             }, function(response) {
+
+//             });
+
+
+//             Util.httpRequest('https://api.shanbay.com/bdc/search/?word=' + window.getSelection().toString().trim(), function(res) {
+//                 var json;
+//                 if (typeof res === 'object') {
+//                     json = res;
+//                 } else {
+//                     json = eval("(" + res + ")");
+//                 }
+
+//                 var boxContainer = document.createElement("div");
+
+//                 var speaker = document.createElement("audio");
+
+//                 var meaning = document.createElement("p");
+
+//                 boxContainer.appendChild(speaker);
+
+//                 boxContainer.appendChild(meaning);
+//                 boxContainer.className = "word-box";
+
+//                 document.body.appendChild(boxContainer);
+
+
+//             });
+//         }
+//     })
+
+
+// }
+// }
 
 
 // (function() {
